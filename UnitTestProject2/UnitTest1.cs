a61e22b687eb6f99a35caaf731ac8072ed980fa1
Rashi Maheshwar <Rashi Maheshwar@RASHI>
2021-10-16 21:23:06 +0530

    Add project files.

---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTestProject2
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
